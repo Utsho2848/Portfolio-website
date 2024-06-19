8da47dcf8af407f8e982be0fbb8ed6a00d4993d5
Md Shohanur Rahman <utsho2848@gmail.com>
2024-06-19 21:39:42 +0600

    about section complete

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './about.css'
 import ME from '../../assets/assets/me-about.jpg'
 import { FaAward } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+import { FaFileCode } from "react-icons/fa";
 
 const About = () => {
     return (
@@ -22,7 +24,23 @@ const About = () => {
                             <h5>Experience</h5>
                             <small>3+ Years Working</small>
                         </article>
+
+
+                        <article className="about_card">
+                            <FaRegUser className='about_icon' />
+                            <h5>Clients</h5>
+                            <small>200+ World Wide</small>
+                        </article>
+
+
+                        <article className="about_card">
+                            <FaFileCode className='about_icon' />
+                            <h5>Projects</h5>
+                            <small>20+ Completed</small>
+                        </article>
                     </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aspernatur nisi corrupti. Sapiente quae commodi distinctio voluptates dicta architecto omnis.</p>
+                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
                 </div>
             </div>
         </section>
