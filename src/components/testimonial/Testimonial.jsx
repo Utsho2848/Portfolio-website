e2b64c92d4fe1130d573fd42eb03229dc3ab004e
Md Shohanur Rahman <utsho2848@gmail.com>
2025-11-08 20:01:15 +0600

    final fixed version

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import './testimonial.css'
-
-
+import './testimonial.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { Pagination, Autoplay } from 'swiper/modules';
 
-import TestimonialData from '../../data/testimonials.json';
+import testimonials from '../../data/testimonials.json';
 
+const Testimonial = () => {
+  return (
+    <section id="testimonials">
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
 
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        loop={true}
+        className="container testimonials_container"
+      >
+        {testimonials.map(({ id, image, name, review }) => (
+          <SwiperSlide key={id} className="testimonial">
+            <div className="client_avatar">
+              <img src={`/assets/${image}`} alt={name} />
+            </div>
+            <h5 className="client_name">{name}</h5>
+            <small className="client_review">{review}</small>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
 
-const Testimonial=()=>{
-{TestimonialData.map(({ id, image, name, review }) => (
-  <SwiperSlide key={id} className="testimonial">
-    <div className="client_avatar">
-      <img src={`/assets/${image}`} alt={name} />
-    </div>
-    <h5 className='client_name'>{name}</h5>
-    <small className='client_review'>{review}</small>
-  </SwiperSlide>
-))}
- }
 export default Testimonial;
